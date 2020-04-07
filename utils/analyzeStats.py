4cd55766f50b7cc8908cf553b5dd20be95253777
gsantoni <santonigl@gmail.com>
2020-04-07 15:05:48 +0200

    Sorted mx structures by rfree and EM by resolution

--- utils/analyzeStats.py
+++ utils/analyzeStats.py
@@ -5,9 +5,10 @@ conn = sqlite3.connect("stats.db")
 
 c = conn.cursor()
 
+outlist = open('mxList.txt', 'w') 
+print('path, Rfree', file=outlist)
 with conn:
-    for row in c.execute('SELECT * FROM stats WHERE method=?',('ELECTRON MICROSCOPY',)):
-        print(row)
-        print('hi!')
+    for row in c.execute('SELECT * FROM stats WHERE method=? ORDER BY protein, rfree',('X-RAY DIFFRACTION',)):
+        print(row[1],row[-1], file=outlist)
 print('Done!')
 
