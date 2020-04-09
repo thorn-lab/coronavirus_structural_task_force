dadba18017f71de4f82ca0d407fd794314911df7
Gianluca Santoni <gsantoni@debian10mx.esrf.fr>
2020-04-09 17:32:07 +0200

    Added RMS for nmr structures and check for isolde existing to the database.

--- utils/populateDatabase.py
+++ utils/populateDatabase.py
@@ -10,7 +10,9 @@ c.execute("""CREATE TABLE stats (pdbid text, datapath text,
                                  protein text,
                                  virus text,
                                  method text, 
+                                 hasRerefinement int,
                                  resolution real, 
+                                 rmsd real,
                                  rwork real, 
                                  rfree real)""")
 
@@ -25,6 +27,10 @@ def fillTheDB(workdir):
             for f in dirs:
                 folder = os.path.join(root,f)
                 l = root.split('/')
+                if os.path.exists(folder+'/isolde'):
+                    hasRerefinement = 1
+                else:
+                    hasRerefinement = 0
                 with conn:
                     try:
                         d = cif.read(folder+'/'+f+'.cif')
@@ -39,55 +45,57 @@ def fillTheDB(workdir):
                     method = b.str(0)
                     rfree = 0
                     rwork = 0
+                    rmsd = 0
                     resolution = 0
                     if method == 'X-RAY DIFFRACTION':
-                        print(method)
                         rfree = float(block.find_value('_refine.ls_R_factor_R_free'))
                         rwork = float(block.find_value('_refine.ls_R_factor_R_work'))
+                        rmsd = None
                         #fsc = mmcif_dict['_entry_for_fsc']
                         try:
                             resolution = float(block.find_value('_reflns.d_resolution_high'))
                         except:
                             resolution = None
                     elif method =='ELECTRON MICROSCOPY':
-                        print(method)
                         rfree = None
                         rwork = None
+                        rmsd = None
                         #fsc = mmcif_dict['_entry_for_fsc']
                         resolution = block.find_value('_em_3d_reconstruction.resolution')
                     elif method == 'SOLUTION NMR':
                         rfree = None
                         rwork = None
+                        rmsd = block.find_value('pdbx_nmr_ensemble_rms')
                         #fsc = mmcif_dict['_entry_for_fsc']
                         resolution = None
                     else:
                         print('something terribly wrong here')
-                    parsed_values=(code, path, protein, virus , method, resolution , rwork, rfree)                    
-                    c.execute('INSERT INTO stats VALUES(?,?,?,?,?,?,?,?)',parsed_values)
+                    parsed_values=(code, path, protein, virus , method, hasRerefinement, resolution , rmsd, rwork, rfree)                    
+                    c.execute('INSERT INTO stats VALUES(?,?,?,?,?,?,?,?,?,?)',parsed_values)
 pwd = os.getcwd()
 print(pwd)
 fillTheDB(pwd)
 
 
 
-outlist = open('mxList.txt', 'w') 
-print('path, Rfree', file=outlist)
-with conn:
-    for row in c.execute('SELECT * FROM stats WHERE method=? ORDER BY protein, rfree',('X-RAY DIFFRACTION',)):
-        print(row[1],row[-1], file=outlist)
-print('Done!')
+# outlist = open('mxList.txt', 'w') 
+# print('path, Rfree', file=outlist)
+# with conn:
+#     for row in c.execute('SELECT * FROM stats WHERE method=? ORDER BY protein, rfree',('X-RAY DIFFRACTION',)):
+#         print(row[1],row[-1], file=outlist)
+# print('Done!')
 
-emlist = open('emList.txt', 'w') 
-print('path, resolution', file=emlist)
-with conn:
-    for row in c.execute('SELECT * FROM stats WHERE method=? ORDER BY protein, resolution',('ELECTRON MICROSCOPY',)):
-        print(row[1],row[-3], file=emlist)
-print('Done!')
+# emlist = open('emList.txt', 'w') 
+# print('path, resolution', file=emlist)
+# with conn:
+#     for row in c.execute('SELECT * FROM stats WHERE method=? ORDER BY protein, resolution',('ELECTRON MICROSCOPY',)):
+#         print(row[1],row[-4], file=emlist)
+# print('Done!')
 
 
-full_db = open('stats.csv', 'w') 
-print('pdb, path, protein, virus , method, resolution , rwork, rfree', file=full_db)
-with conn:
-    for row in c.execute('SELECT * FROM stats ORDER BY protein'):
-        print(str(row), file=full_db)
-print('Done!')
+# full_db = open('stats.csv', 'w') 
+# print('pdb, path, protein, virus , method, resolution ,rmsd rwork, rfree', file=full_db)
+# with conn:
+#     for row in c.execute('SELECT * FROM stats ORDER BY protein'):
+#         print(str(row), file=full_db)
+# print('Done!')
