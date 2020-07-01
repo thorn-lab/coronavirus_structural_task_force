87cc83051d22fadce2d0547776cecb1f5a0af426
gsantoni <santonigl@gmail.com>
2020-07-01 23:59:44 +0200

    retrieved new pdb redo data

---
+++ pdb/3c_like_proteinase/SARS-CoV-2/6xg2/validation/pdb-redo/6xg2_final.py
@@ -0,0 +1,26 @@
+# Guided tour for Coot written by PDB_REDO
+# Please cite: Joosten et al. 'PDB_REDO: constructive validation, more than just looking for errors' Acta Cryst., D68, 484-496 (2012)
+interesting_things_gui ("PDB_REDO results",[
+["Changed rotamer GLU A  47 " ,  13.310 ,   3.825 ,  35.643],
+["Changed rotamer LEU A  50 " ,   8.808 ,  -0.763 ,  35.978],
+["Changed rotamer GLN A  69 " ,  10.698 ,  19.362 ,  14.972],
+["Changed rotamer ASN A  72 " ,  13.083 ,  23.281 ,   8.666],
+["H-bond flip GLN A  74 " ,  10.967 ,  24.853 ,  16.879],
+["H-bond flip GLN A 107 " , -15.883 ,  -3.145 ,  14.930],
+["Changed rotamer TYR A 154 " , -16.142 ,  12.295 ,   0.563],
+["H-bond flip HIS A 164 " ,   0.995 ,   2.562 ,  22.145],
+["H-bond flip ASN A 180 " ,  -9.791 ,   4.252 ,  24.444],
+["Changed rotamer LYS A 236 " , -14.152 , -27.219 ,  13.443],
+["H-bond flip ASN A 277 " ,  -6.144 , -29.414 ,  -4.134],
+["Added OXT GLN A 306 " , -11.625 ,  16.371 ,  -1.986],
+["Changed rotamer GLN A 306 " , -12.385 ,  15.028 ,  -4.430],
+["Removed HOH A 576 " , -16.643 , -19.748 ,  15.662],
+["Removed HOH A 619 " , -18.926 , -20.409 ,  15.139],
+["Removed HOH A 677 " ,  12.705 ,  10.525 ,  28.415],
+["Removed HOH A 682 " ,  -3.153 ,  28.931 ,  17.008],
+["Removed HOH A 689 " ,  -6.960 ,  26.406 ,  15.468],
+["Removed HOH A 694 " ,   6.784 ,  -9.554 ,  28.143],
+["Removed HOH A 705 " ,   3.493 ,   1.253 ,  25.889],
+["Removed HOH A 713 " ,  -6.790 , -25.057 ,  12.131],
+["Removed HOH A 716 " , -20.980 , -20.047 ,  16.601],
+])
