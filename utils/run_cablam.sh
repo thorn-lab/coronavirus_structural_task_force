0e8d60f19c6ada0d827f0ad41aff24fe5181fde0
gsantoni <santonigl@gmail.com>
2020-11-23 15:43:34 +0100

    New xtriage run

---
+++ utils/run_cablam.sh
@@ -0,0 +1,20 @@
+cd ../pdb
+
+base=$(pwd)
+
+
+for x in */*/* ;
+do
+    c=`basename $x`;
+    echo $x
+    e="${c,,}" ;
+    cd $x ; 
+    if [ -f  validation/molprobity/cablam.out ] ; then
+	echo 'nothing to do here'
+    else
+	phenix.cablam $c.pdb > validation/molprobity/cablam.out
+	
+    fi
+    cd $base
+
+done
