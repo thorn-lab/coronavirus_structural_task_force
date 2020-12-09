86a41ccf68303e3191032ddcbe87315649669481
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2020-12-09 09:59:52 +0100

    Update ID_getter.py

--- utils/Update_pipeline/ID_getter.py
+++ utils/Update_pipeline/ID_getter.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 import json
 yesterday = date.today() - timedelta(days=1)
 time = yesterday.strftime('%y-%m-%d')
-time = "2020-12-01"
+time = "2020-12-08"
 
 
 import os.path as path
