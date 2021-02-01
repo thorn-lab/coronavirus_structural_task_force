883e306860a3f13a56d7c676530bfb794b41e443
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2021-02-01 18:40:28 +0100

    New RMSD values

--- utils/Update_pipeline/RMSD_WIP.py
+++ utils/Update_pipeline/RMSD_WIP.py
@@ -12,7 +12,6 @@ abc_lst = string.ascii_uppercase
 import prody as pry
 pry.confProDy(verbosity='none')
 
-
 def main (id_dict, path):
     #RMSD is not done for 3c_like_proteinase, surface_glycoprotein, nsp3
     #for protein in all_proteins:
@@ -20,11 +19,8 @@ def main (id_dict, path):
     pdb_id = pdb_id.read().split("\n")
     for protein in id_dict:
         #Here exceptions can be added, e.g. for proteins which have to many entries
-        if protein == "surface_glycoprotein" or protein == "3c_like_proteinase":
-            pass
-        else:
-            repo_path = path+"/"+protein
-            file_walker(protein, pdb_id, repo_path+"/SARS-CoV-2/", "SARS-CoV-2")
+        repo_path = path+"/"+protein
+        file_walker(protein, pdb_id, repo_path+"/SARS-CoV-2/", "SARS-CoV-2")
 
 def file_walker(protein, pdb_id, repo_path, taxo):
     protein_id = []
@@ -33,12 +29,12 @@ def file_walker(protein, pdb_id, repo_path, taxo):
             if dirpath.endswith(key):
                 protein_id.append(key)
 
-    if len(protein_id) != 0:
+    if len(protein_id) > 1:
         matrix_maker(protein, protein_id, repo_path)
     else: pass
 
 def rmsdler (pdb1, pdb2, doc):
-    rmsd_lst = []
+    rmsd_lst, comb_lst, atom_lst = [], [], []
     combi_lst = abc_lst[:max(len(pdb1),len(pdb2))]
     iter_chain = np.asarray(list(itertools.permutations(combi_lst,2)))
     for comb in iter_chain:
@@ -49,10 +45,13 @@ def rmsdler (pdb1, pdb2, doc):
             sup = gm.calculate_superposition(chain1, chain2, ptype, gm.SupSelect.CaP)
             rmsd = round(sup.rmsd , 3)
 
+            atom_lst.append(sup.count)
+            comb_lst.append(comb)
             rmsd_lst.append(rmsd)
             doc.write("Chain[{}] superposed to Chain[{}]: {} \n".format(comb[0], comb[1],str(rmsd)))
         except ValueError: pass
-    if rmsd_lst != []: return min(rmsd_lst)
+    if rmsd_lst != [] and comb_lst != [] and atom_lst != []:
+        return min(rmsd_lst),  comb_lst[rmsd_lst.index(min(rmsd_lst))], atom_lst[rmsd_lst.index(min(rmsd_lst))]
 
 def matrix_maker (protein, pdb_id, repo_path):
     doc = open(repo_path + "{}_RMSD_by_chain.txt".format(protein), "w+")
@@ -62,24 +61,53 @@ def matrix_maker (protein, pdb_id, repo_path):
               "If you want to looking for the similarity of the proteins we recommend using <protein>_best_RMSD")
 
     pdb_entrys = {}
+    #Parse every PDB entry in a dict
     for id in pdb_id:
-        pdb_entrys[id] = gm.read_structure(repo_path + id + "/{}.pdb".format(id))[0]
+        try: pdb_entrys[id] = gm.read_structure(repo_path + id + "/{}.pdb".format(id))[0]
+        except RuntimeError: pass
 
     #create product of all id combinations
     iter_id= itertools.combinations(pdb_id, 2)
     iter_id = np.asarray(list(iter_id))
 
-    id_arr = pd.DataFrame(columns=["PDB-1","PDB-2","RMSD"])
+    #Transfer the combinations to a pandas DataFrame
+    id_arr = pd.DataFrame(columns=["PDB-1", "Chain-1","PDB-2","Chain-2","RMSD", "Aligned atoms"])
     id_arr["PDB-1"] = iter_id[:,0]
     id_arr["PDB-2"] = iter_id[:,1]
-    rmsd_lst = []
+    rmsd_lst, best_chain_lst, n_atom_lst = [], [], []
 
     for i in range(len(id_arr["PDB-1"])):
         doc.write("\n>{}-{}<\n".format(str(id_arr["PDB-1"][i]),str(id_arr["PDB-2"][i])))
-        rmsd = rmsdler(pdb_entrys[id_arr["PDB-1"][i]],pdb_entrys[id_arr["PDB-2"][i]],doc)
-        rmsd_lst.append(rmsd)
+        try:
+            result = rmsdler(pdb_entrys[id_arr["PDB-1"][i]],pdb_entrys[id_arr["PDB-2"][i]],doc)
+            if result != None:
+                rmsd_lst.append(result[0])
+                best_chain_lst.append(result[1])
+                n_atom_lst.append(result[2])
+            else:
+                rmsd_lst.append(None)
+                best_chain_lst.append([None,None])
+                n_atom_lst.append(None)
+        except KeyError:
+            rmsd_lst.append(None)
+            best_chain_lst.append([None,None])
+            n_atom_lst.append(None)
 
     id_arr["RMSD"] = np.asarray(rmsd_lst)
+    id_arr["Aligned atoms"] = np.asarray(n_atom_lst)
+    id_arr["Chain-1"] = np.asarray(best_chain_lst)[:,0]
+    id_arr["Chain-2"] = np.asarray(best_chain_lst)[:,1]
+
+    #this duplicates the table invers so the iteration-type changes from combination to permutation without calculation
+    inv_id_arr = pd.DataFrame(columns=["PDB-1","Chain-1","PDB-2","Chain-2","RMSD","Aligned atoms"])
+    inv_id_arr["PDB-1"] = id_arr["PDB-2"]
+    inv_id_arr["PDB-2"] = id_arr["PDB-1"]
+    inv_id_arr["RMSD"] = id_arr["RMSD"]
+    inv_id_arr["Aligned atoms"] = id_arr["Aligned atoms"]
+    inv_id_arr["Chain-1"] = id_arr["Chain-2"]
+    inv_id_arr["Chain-2"] = id_arr["Chain-1"]
+    id_arr = id_arr.append(inv_id_arr)
+
     id_arr = id_arr.sort_values(by=['RMSD'])
     id_arr.to_excel("{}{}_best_RMSD.xlsx".format(repo_path, protein), index=False)
     doc.close()
@@ -118,5 +146,28 @@ def heatmap (matrix, color, pdb_id, protein, repo_path):
 
 repo_path = osp.abspath(osp.join(__file__ ,"../../..","pdb"))
 id_dict = {}
+id_dict["endornase"] = []
+id_dict["3c_like_proeinase"] = []
+id_dict["exonuclease-nsp10"] = []
+id_dict["helicase"] = []
+id_dict["hypothetical_protein_sars7a"] = []
+id_dict["leader_protein"] = []
+id_dict["methyltransferase-nsp10"] = []
+id_dict["nsp10"] = []
 id_dict["nsp3"] = []
+id_dict["nsp7"] = []
+id_dict["nsp8-nsp7"] = []
+id_dict["nsp8"] = []
+id_dict["nsp9"] = []
+id_dict["nucleocapsid_protein"] = []
+id_dict["orf3a_protein"] = []
+id_dict["orf8"] = []
+id_dict["orf9b"] = []
+id_dict["protein_e"] = []
+id_dict["rna_polymerase-nsp7-nsp8"] = []
+id_dict["rna_polymerase-nsp8"] = []
+id_dict["rna"] = []
+id_dict["helicase-rna_polymerase-nsp7-nsp8"] = []
+id_dict["rna_polymerase"] = []
+id_dict["surface_glycoprotein"] = []
 main(id_dict, repo_path)
