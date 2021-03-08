398b55ecacab08c44436e01b3945384ef1bc64d5
Gianluca Santoni <santonigl@gmail.com>
2021-03-08 16:41:39 +0100

    retrieved new pdb redo data

---
+++ pdb/orf7a/SARS-CoV-2/7ci3/validation/pdb-redo/7ci3_final.py
@@ -0,0 +1,16 @@
+# Guided tour for Coot written by PDB_REDO
+# Please cite: Joosten et al. 'PDB_REDO: constructive validation, more than just looking for errors' Acta Cryst., D68, 484-496 (2012)
+interesting_things_gui ("PDB_REDO results",[
+["Changed rotamer GLU A  16 " ,   7.977 ,   2.642 ,   5.900],
+["Changed rotamer LEU A  30 " , -15.031 ,  -0.756 ,   4.102],
+["Changed rotamer LYS A  32 " ,  -9.961 ,   5.494 ,   5.992],
+["Changed rotamer CYS A  35 " ,  -3.334 ,   9.084 ,   1.619],
+["Changed rotamer ASN A  43 " ,  -1.710 ,  -8.023 ,  -6.038],
+["Changed rotamer PRO A  45 " ,  -8.728 ,  -2.967 ,  -9.728],
+["Changed rotamer ASP A  51 " , -16.528 ,   8.963 ,   3.213],
+["Changed rotamer LEU A  56 " ,  -9.705 ,  -4.972 ,  -1.506],
+["Changed rotamer ARG A  80 " ,  -8.959 , -15.877 ,   8.984],
+["Removed HOH A 103 " ,  -2.867 ,  -6.377 ,  -3.259],
+["Removed HOH A 104 " , -13.913 ,   3.603 ,   8.046],
+["Removed HOH A 108 " ,   2.645 ,  -5.787 ,   8.302],
+])
