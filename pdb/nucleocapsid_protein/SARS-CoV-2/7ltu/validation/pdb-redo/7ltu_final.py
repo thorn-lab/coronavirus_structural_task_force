f6217778837d28c7f5794b53742f5cf808c6fb32
Gianluca Santoni <santonigl@gmail.com>
2021-03-18 12:30:25 +0100

    retrieved new pdb redo data

---
+++ pdb/nucleocapsid_protein/SARS-CoV-2/7ltu/validation/pdb-redo/7ltu_final.py
@@ -0,0 +1,4 @@
+# Guided tour for Coot written by PDB_REDO
+# Please cite: Joosten et al. 'PDB_REDO: constructive validation, more than just looking for errors' Acta Cryst., D68, 484-496 (2012)
+interesting_things_gui ("PDB_REDO results",[
+])
